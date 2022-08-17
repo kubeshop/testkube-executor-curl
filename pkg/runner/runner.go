0e6cbc9b9f417f07f9121aa36eab5296ca3ae486
Vladislav Sukhin <vladislav@kubeshop.io>
2022-08-17 09:18:27 +0300

    fix: support git credentials (#41)

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/kelseyhightower/envconfig"
+
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 	"github.com/kubeshop/testkube/pkg/executor"
 	"github.com/kubeshop/testkube/pkg/executor/content"
@@ -15,23 +17,50 @@ import (
 	"go.uber.org/zap"
 )
 
+// Params ...
+type Params struct {
+	Endpoint        string // RUNNER_ENDPOINT
+	AccessKeyID     string // RUNNER_ACCESSKEYID
+	SecretAccessKey string // RUNNER_SECRETACCESSKEY
+	Location        string // RUNNER_LOCATION
+	Token           string // RUNNER_TOKEN
+	Ssl             bool   // RUNNER_SSL
+	ScrapperEnabled bool   // RUNNER_SCRAPPERENABLED
+	GitUsername     string // RUNNER_GITUSERNAME
+	GitToken        string // RUNNER_GITTOKEN
+}
+
 const CurlAdditionalFlags = "-is"
 
 // CurlRunner is used to run curl commands.
 type CurlRunner struct {
+	Params  Params
 	Fetcher content.ContentFetcher
 	Log     *zap.SugaredLogger
 }
 
 func NewCurlRunner() *CurlRunner {
+	var params Params
+	err := envconfig.Process("runner", &params)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	return &CurlRunner{
 		Log:     log.DefaultLogger,
+		Params:  params,
 		Fetcher: content.NewFetcher(""),
 	}
 }
 
 func (r *CurlRunner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
 	var runnerInput CurlRunnerInput
+	if r.Params.GitUsername != "" && r.Params.GitToken != "" {
+		if execution.Content != nil && execution.Content.Repository != nil {
+			execution.Content.Repository.Username = r.Params.GitUsername
+			execution.Content.Repository.Token = r.Params.GitToken
+		}
+	}
 
 	path, err := r.Fetcher.Fetch(execution.Content)
 	if err != nil {
