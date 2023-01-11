3369daec00b492c20978322b43ce131cea14ad8d
Lilla Vass <lilla@kubeshop.io>
2023-01-11 18:35:34 +0100

    ci: update testkube dependency

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kubeshop/testkube-executor-curl/pkg/runner"
 	"github.com/kubeshop/testkube/pkg/executor/agent"
+	"github.com/kubeshop/testkube/pkg/ui"
 )
 
 func main() {
-	agent.Run(runner.NewCurlRunner(), os.Args)
+	r, err := runner.NewCurlRunner()
+	if err != nil {
+		panic(fmt.Errorf("%s Could not run cURL tests: %w", ui.IconCross, err))
+	}
+
+	agent.Run(r, os.Args)
 }
--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -9,9 +9,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/kelseyhightower/envconfig"
-
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
+	"github.com/kubeshop/testkube/pkg/envs"
 	"github.com/kubeshop/testkube/pkg/executor"
 	contentPkg "github.com/kubeshop/testkube/pkg/executor/content"
 	outputPkg "github.com/kubeshop/testkube/pkg/executor/output"
@@ -22,51 +21,31 @@ import (
 	"go.uber.org/zap"
 )
 
-// Params ...
-type Params struct {
-	Endpoint        string // RUNNER_ENDPOINT
-	AccessKeyID     string // RUNNER_ACCESSKEYID
-	SecretAccessKey string // RUNNER_SECRETACCESSKEY
-	Location        string // RUNNER_LOCATION
-	Token           string // RUNNER_TOKEN
-	Ssl             bool   // RUNNER_SSL
-	ScrapperEnabled bool   // RUNNER_SCRAPPERENABLED
-	GitUsername     string // RUNNER_GITUSERNAME
-	GitToken        string // RUNNER_GITTOKEN
-	DataDir         string // RUNNER_DATADIR
-}
-
 const CurlAdditionalFlags = "-is"
 
 // CurlRunner is used to run curl commands.
 type CurlRunner struct {
-	Params  Params
+	Params  envs.Params
 	Fetcher contentPkg.ContentFetcher
 	Log     *zap.SugaredLogger
 }
 
-func NewCurlRunner() *CurlRunner {
-	outputPkg.PrintEvent(fmt.Sprintf("%s Preparing test runner", ui.IconTruck))
-	var params Params
-
-	outputPkg.PrintEvent(fmt.Sprintf("%s Reading environment variables...", ui.IconWorld))
-	err := envconfig.Process("runner", &params)
+func NewCurlRunner() (*CurlRunner, error) {
+	outputPkg.PrintLog(fmt.Sprintf("%s Preparing test runner", ui.IconTruck))
+	params, err := envs.LoadTestkubeVariables()
 	if err != nil {
-		outputPkg.PrintEvent(fmt.Sprintf("%s Failed to read environment variables: %s", ui.IconCross, err.Error()))
-		panic(err.Error())
+		return nil, fmt.Errorf("could not initialize cURL runner variables: %w", err)
 	}
-	outputPkg.PrintEvent(fmt.Sprintf("%s Environment variables read successfully", ui.IconCheckMark))
-	printParams(params)
 
 	return &CurlRunner{
 		Log:     log.DefaultLogger,
 		Params:  params,
 		Fetcher: contentPkg.NewFetcher(""),
-	}
+	}, nil
 }
 
 func (r *CurlRunner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
-	outputPkg.PrintEvent(fmt.Sprintf("%s Preparing for test run", ui.IconTruck))
+	outputPkg.PrintLog(fmt.Sprintf("%s Preparing for test run", ui.IconTruck))
 	var runnerInput CurlRunnerInput
 	if r.Params.GitUsername != "" || r.Params.GitToken != "" {
 		if execution.Content != nil && execution.Content.Repository != nil {
@@ -75,13 +54,10 @@ func (r *CurlRunner) Run(execution testkube.Execution) (result testkube.Executio
 		}
 	}
 
-	outputPkg.PrintEvent(fmt.Sprintf("%s Fetching test content from %s...", ui.IconBox, execution.Content.Type_))
 	path, err := r.Fetcher.Fetch(execution.Content)
 	if err != nil {
-		outputPkg.PrintEvent(fmt.Sprintf("%s Could not fetch test content: %s", ui.IconCross, err.Error()))
 		return result, err
 	}
-	outputPkg.PrintEvent(fmt.Sprintf("%s Test content fetched to path %s", ui.IconCheckMark, path))
 
 	if !execution.Content.IsFile() {
 		return result, testkube.ErrTestContentTypeNotFile
@@ -100,14 +76,19 @@ func (r *CurlRunner) Run(execution testkube.Execution) (result testkube.Executio
 	envManager := secret.NewEnvManagerWithVars(execution.Variables)
 	envManager.GetVars(envManager.Variables)
 	variables := testkube.VariablesToMap(envManager.Variables)
+
+	outputPkg.PrintLog(fmt.Sprintf("%s Filling in the input templates", ui.IconKey))
 	err = runnerInput.FillTemplates(variables)
 	if err != nil {
+		outputPkg.PrintLog(fmt.Sprintf("%s Failed to fill in the input templates: %s", ui.IconCross, err.Error()))
 		r.Log.Errorf("Error occured when resolving input templates %s", err)
 		return result.Err(err), nil
 	}
+	outputPkg.PrintLog(fmt.Sprintf("%s Successfully filled the input templates", ui.IconCheckMark))
 
 	command := runnerInput.Command[0]
 	if command != "curl" {
+		outputPkg.PrintLog(fmt.Sprintf("%s you can run only `curl` commands with this executor but passed: `%s`", ui.IconCross, command))
 		return result, fmt.Errorf("you can run only `curl` commands with this executor but passed: `%s`", command)
 	}
 
@@ -121,12 +102,10 @@ func (r *CurlRunner) Run(execution testkube.Execution) (result testkube.Executio
 		runPath = filepath.Join(r.Params.DataDir, "repo", execution.Content.Repository.WorkingDir)
 	}
 
-	outputPkg.PrintEvent(fmt.Sprintf("%s executing test\n\t$ %s %s", ui.IconMicroscope, command, strings.Join(args, " ")))
 	output, err := executor.Run(runPath, command, envManager, args...)
 	output = envManager.Obfuscate(output)
 
 	if err != nil {
-		outputPkg.PrintEvent(fmt.Sprintf("%s Error occured when running a command %s", ui.IconCross, err.Error()))
 		r.Log.Errorf("Error occured when running a command %s", err)
 		return result.Err(err), nil
 	}
@@ -135,27 +114,27 @@ func (r *CurlRunner) Run(execution testkube.Execution) (result testkube.Executio
 	result.Output = outputString
 	responseStatus, err := getResponseCode(outputString)
 	if err != nil {
-		outputPkg.PrintEvent(fmt.Sprintf("%s Test run failed: %s", ui.IconCross, err.Error()))
+		outputPkg.PrintLog(fmt.Sprintf("%s Test run failed: %s", ui.IconCross, err.Error()))
 		return result.Err(err), nil
 	}
 
 	expectedStatus, err := strconv.Atoi(runnerInput.ExpectedStatus)
 	if err != nil {
-		outputPkg.PrintEvent(fmt.Sprintf("%s Test run failed: cannot process expected status: %s", ui.IconCross, err.Error()))
+		outputPkg.PrintLog(fmt.Sprintf("%s Test run failed: cannot process expected status: %s", ui.IconCross, err.Error()))
 		return result.Err(fmt.Errorf("cannot process expected status %s", runnerInput.ExpectedStatus)), nil
 	}
 
 	if responseStatus != expectedStatus {
-		outputPkg.PrintEvent(fmt.Sprintf("%s Test run failed: cannot process expected status: %s", ui.IconCross, err.Error()))
+		outputPkg.PrintLog(fmt.Sprintf("%s Test run failed: cannot process expected status: %s", ui.IconCross, err.Error()))
 		return result.Err(fmt.Errorf("response status don't match expected %d got %d", expectedStatus, responseStatus)), nil
 	}
 
 	if !strings.Contains(outputString, runnerInput.ExpectedBody) {
-		outputPkg.PrintEvent(fmt.Sprintf("%s Test run failed: response doesn't contain body: %s", ui.IconCross, runnerInput.ExpectedBody))
+		outputPkg.PrintLog(fmt.Sprintf("%s Test run failed: response doesn't contain body: %s", ui.IconCross, runnerInput.ExpectedBody))
 		return result.Err(fmt.Errorf("response doesn't contain body: %s", runnerInput.ExpectedBody)), nil
 	}
 
-	outputPkg.PrintEvent(fmt.Sprintf("%s Test run succeeded", ui.IconCheckMark))
+	outputPkg.PrintLog(fmt.Sprintf("%s Test run succeeded", ui.IconCheckMark))
 
 	return testkube.ExecutionResult{
 		Status: testkube.ExecutionStatusPassed,
@@ -175,27 +154,4 @@ func getResponseCode(curlOutput string) (int, error) {
 // GetType returns runner type
 func (r *CurlRunner) GetType() runner.Type {
 	return runner.TypeMain
-}
-
-// printParams shows the read parameters in logs
-func printParams(params Params) {
-	outputPkg.PrintLog(fmt.Sprintf("RUNNER_ENDPOINT=\"%s\"", params.Endpoint))
-	printSensitiveParam("RUNNER_ACCESSKEYID", params.AccessKeyID)
-	printSensitiveParam("RUNNER_SECRETACCESSKEY", params.SecretAccessKey)
-	outputPkg.PrintLog(fmt.Sprintf("RUNNER_LOCATION=\"%s\"", params.Location))
-	printSensitiveParam("RUNNER_TOKEN", params.Token)
-	outputPkg.PrintLog(fmt.Sprintf("RUNNER_SSL=%t", params.Ssl))
-	outputPkg.PrintLog(fmt.Sprintf("RUNNER_SCRAPPERENABLED=\"%t\"", params.ScrapperEnabled))
-	outputPkg.PrintLog(fmt.Sprintf("RUNNER_GITUSERNAME=\"%s\"", params.GitUsername))
-	printSensitiveParam("RUNNER_GITTOKEN", params.GitToken)
-	outputPkg.PrintLog(fmt.Sprintf("RUNNER_DATADIR=\"%s\"", params.DataDir))
-}
-
-// printSensitiveParam shows in logs if a parameter is set or not
-func printSensitiveParam(name string, value string) {
-	if len(value) == 0 {
-		outputPkg.PrintLog(fmt.Sprintf("%s=\"\"", name))
-	} else {
-		outputPkg.PrintLog(fmt.Sprintf("%s=\"********\"", name))
-	}
 }
